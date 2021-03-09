01b459e28b2fab0c650bddb3a1e090a6640844ea
“inziani” <ValentineRobai.Inziani@gmail.com>
2021-03-09 17:05:22 +0300

    Add component details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', 'https://pixabay.com/vectors/recipe-label-icon-symbol-spoon-575434/'),
+    new Recipe('A test recipe', 'This is simply a test', 'https://pixabay.com/photos/recipe-dish-lunch-nutrition-food-3889916/')
+  ];
 
   constructor() { }
 
